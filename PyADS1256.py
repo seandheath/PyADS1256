7987576631a1aafba56136528333ad98ea826e32
Sean Heath <seanheath87@gmail.com>
2016-02-19 14:11:22 -0500

    Ready for initial testing on Pi

--- PyADS1256.py
+++ PyADS1256.py
@@ -1,6 +1,26 @@
 import PyBCM2835 as bcm
 import time
 
+"""
+
+This is a list of the commands accepted by the ADS1256
+
+WAKEUP Completes SYNC and Exits Standby Mode 0000 0000 (00h)
+RDATA Read Data 0000 0001 (01h)
+RDATAC Read Data Continuously 0000 0011 (03h)
+SDATAC Stop Read Data Continuously 0000 1111 (0Fh)
+done - RREG Read from REG rrr 0001 rrrr (1xh) 0000 nnnn
+done - WREG Write to REG rrr 0101 rrrr (5xh) 0000 nnnn
+SELFCAL Offset and Gain Self-Calibration 1111 0000 (F0h)
+SELFOCAL Offset Self-Calibration 1111 0001 (F1h)
+SELFGCAL Gain Self-Calibration 1111 0010 (F2h)
+SYSOCAL System Offset Calibration 1111 0011 (F3h)
+SYSGCAL System Gain Calibration 1111 0100 (F4h)
+SYNC Synchronize the A/D Conversion 1111 1100 (FCh)
+STANDBY Begin Standby Mode 1111 1101 (FDh)
+RESET Reset to Power-Up Values 1111 1110 (FEh)
+WAKEUP Completes SYNC and Exits Standby Mode 1111 1111 (FFh)
+"""
 
 class 
     # The RPI GPIO to use for chip select and ready polling
@@ -11,12 +31,13 @@ class
     SPI_CLOCK_DIVIDER   = bcm.SPI_CLOCK_DIVIDER_8192
     DRDY_TIMEOUT        = 0.5 # Seconds to wait for DRDY when communicating
     DATA_TIMEOUT        = 0.00001 # 10uS delay for sending data
+    SCLK_FREQUENCY      = 7680000 # default clock rate is 7.68MHz
 
     # ADS Chip State
     CURRENT_GAIN    = -1
     CURRENT_DRATE   = -1
 
-    # Register definitions
+    # Register addresses
     REG_STATUS  = 0
     REG_MUX     = 1
     REG_ADCON   = 2
@@ -29,32 +50,49 @@ class
     REG_FSC1    = 9
     REG_FSC2    = 10
 
-    # Gain levels
-    GAIN_1      = 0
-    GAIN_2      = 1
-    GAIN_4      = 2
-    GAIN_6      = 3
-    GAIN_8      = 4
-    GAIN_16     = 5
-    GAIN_32     = 6
+    """
+    DRATE Register: A/D Data Rate Address 0x03
+    The 16 valid Data Rate settings are shown below. Make sure to select a
+    valid setting as the invalid settings may produce unpredictable results.
 
-    # Data rates, samples per second
-    DRATE_30000 = 0
-    DRATE_15000 = 1
-    DRATE_7500  = 2
-    DRATE_3750  = 3
-    DRATE_2000  = 4
-    DRATE_1000  = 5
-    DRATE_500   = 6
-    DRATE_100   = 7
-    DRATE_60    = 8
-    DRATE_50    = 9
-    DRATE_30    = 10
-    DRATE_25    = 11
-    DRATE_15    = 12
-    DRATE_10    = 13
-    DRATE_5     = 15
-    DRATE_2     = 16
+    Bits 7-0 DR[7: 0]: Data Rate Setting(1)
+
+        11110000 = 30,000SPS (default)
+        11100000 = 15,000SPS
+        11010000 = 7,500SPS
+        11000000 = 3,750SPS
+        10110000 = 2,000SPS
+        10100001 = 1,000SPS
+        10010010 = 500SPS
+        10000010 = 100SPS
+        01110010 = 60SPS
+        01100011 = 50SPS
+        01010011 = 30SPS
+        01000011 = 25SPS
+        00110011 = 15SPS
+        00100011 = 10SPS
+        00010011 = 5SPS
+        00000011 = 2.5SPS
+
+        (1) for fCLKIN = 7.68MHz. Data rates scale linearly with fCLKIN
+    """
+    # Data rates
+    DRATE_30000     = 0b11110000 # 30,000SPS (default)
+    DRATE_15000     = 0b11100000 # 15,000SPS
+    DRATE_7500      = 0b11010000 # 7,500SPS
+    DRATE_3750      = 0b11000000 # 3,750SPS
+    DRATE_2000      = 0b10110000 # 2,000SPS
+    DRATE_1000      = 0b10100001 # 1,000SPS
+    DRATE_500       = 0b10010010 # 500SPS
+    DRATE_100       = 0b10000010 # 100SPS
+    DRATE_60        = 0b01110010 # 60SPS
+    DRATE_50        = 0b01100011 # 50SPS
+    DRATE_30        = 0b01010011 # 30SPS
+    DRATE_25        = 0b01000011 # 25SPS
+    DRATE_15        = 0b00110011 # 15SPS
+    DRATE_10        = 0b00100011 # 10SPS
+    DRATE_5         = 0b00010011 # 5SPS
+    DRATE_2_5       = 0b00000011 # 2.5SPS
 
     # Commands
     CMD_WAKEUP  = 0x00 # Completes SYNC and exits standby mode
@@ -72,7 +110,109 @@ class
     CMD_STANDBY = 0xFD # Begin standby mode
     CMD_RESET   = 0xFE # Reset to power-on values
 
+    """
+    Status Register Configuration - logically OR all desired options together
+    to form a 1 byte command and write it to the STATUS register
 
+    STATUS REGISTER - ADDRESS 0x00
+    Bits 7-4 ID3, ID2, ID1, ID0 Factory Programmed Identification Bits 
+    (Read Only)
+
+    Bit 3 ORDER: Data Output Bit Order
+
+        0 = Most Significant Bit First (default)
+        1 = Least Significant Bit First
+
+        Input data is always shifted in most significant byte and bit first.
+        Output data is always shifted out most significant byte first. The
+        ORDER bit only controls the bit order of the output data within the
+        byte.
+
+    Bit 2 ACAL: Auto-Calibration
+
+        0 = Auto-Calibration Disabled (default)
+        1 = Auto-Calibration Enabled
+
+        When Auto-Calibration is enabled, self-calibration begins at the
+        completion of the WREG command that changes the PGA (bits 0-2 of ADCON
+        register), DR (bits 7-0 in the DRATE register) or BUFEN (bit 1 in the
+        STATUS register) values.
+
+    Bit 1 BUFEN: Analog Input Buffer Enable
+
+        0 = Buffer Disabled (default)
+        1 = Buffer Enabled
+
+    Bit 0 DRDY: Data Ready (Read Only)
+
+        This bit duplicates the state of the DRDY pin, which is inverted logic.
+    """
+    STATUS_BUFFER_ENABLE    = 0x02
+    STATUS_AUTOCAL_ENABLE   = 0x04
+    STATUS_ORDER_LSB        = 0x08
+
+
+    """
+    A/D Control Register - Address 0x02
+
+    Bit 7 Reserved, always 0 (Read Only)
+
+    Bits 6-5 CLK1, CLK0: D0/CLKOUT Clock Out Rate Setting
+
+        00 = Clock Out OFF
+        01 = Clock Out Frequency = fCLKIN (default)
+        10 = Clock Out Frequency = fCLKIN/2
+        11 = Clock Out Frequency = fCLKIN/4
+
+        When not using CLKOUT, it is recommended that it be turned off. These
+        bits can only be reset using the RESET pin.
+
+    Bits 4-3 SDCS1, SCDS0: Sensor Detect Current Sources
+
+        00 = Sensor Detect OFF (default)
+        01 = Sensor Detect Current = 0.5μA
+        10 = Sensor Detect Current = 2μA
+        11 = Sensor Detect Current = 10μA
+
+        The Sensor Detect Current Sources can be activated to verify the
+        integrity of an external sensor supplying a signal to the ADS1255/6.
+        A shorted sensor produces a very small signal while an open-circuit
+        sensor produces a very large signal.
+
+    Bits 2-0 PGA2, PGA1, PGA0: Programmable Gain Amplifier Setting
+        000 = 1 (default)
+        001 = 2
+        010 = 4
+        011 = 8
+        100 = 16
+        101 = 32
+        110 = 64
+        111 = 64
+    """
+    # Gain levels
+    AD_GAIN_1      = 0x00
+    AD_GAIN_2      = 0x01
+    AD_GAIN_4      = 0x02
+    AD_GAIN_8      = 0x03
+    AD_GAIN_16     = 0x04
+    AD_GAIN_32     = 0x05
+    AD_GAIN_64     = 0x06
+
+    # Sensor Detect Current Sources
+    AD_SDCS_500pA   = 0x08
+    AD_SDCS_2uA     = 0x10
+    AD_SDCS_10uA    = 0x18
+
+    # Clock divider
+    AD_CLK_EQUAL    = 0x20
+    AD_CLK_HALF     = 0x40
+    AD_CLK_FOURTH   = 0x60
+
+    def cs_low(self):
+        bcm.gpio_write(self.CS, bcm.LOW) 
+
+    def cs_high(self):
+        bcm.gpio_write(self.CS, bcm.HIGH)
 
     def init(self):
         """
@@ -107,7 +247,7 @@ class
 
     def SendByte(self, cmd):
         """
-        Sends a byte to the indicated command register of the ads chip
+        Sends a byte to the SPI bus
         """
         bcm.spi_transfer(cmd)
 
@@ -122,7 +262,17 @@ class
     def DataDelay(self):
         """
         Delay from last SCLK edge to first SCLK rising edge
+
+        Master clock rate is typically 7.68MHz, this is adjustable through the
+        SCLK_FREQUENCY variable
+        
+        Datasheet states that the delay between requesting data and reading the
+        bus must be minimum 50x SCLK period, this function reads data after
+        60 x SCLK period.
         """
+        timeout = (60 / self.SCLK_FREQUENCY)
+
+
         start = time.time()
         elapsed = time.time() - start
 
@@ -133,40 +283,105 @@ class
 
     def ReadReg(self, reg):
         """
-        Read the provided register
-        :returns: the contents of the provided register
+        Read the provided register, implements:
+        
+        RREG: Read from Registers
+
+        Description: Output the data from up to 11 registers starting with the
+        register address specified as part of the command. The number of
+        registers read will be one plus the second byte of the command. If the
+        count exceeds the remaining registers, the addresses will wrap back to
+        the beginning.
+
+        1st Command Byte: 0001 rrrr where rrrr is the address of the first
+        register to read.
+
+        2nd Command Byte: 0000 nnnn where nnnn is the number of bytes to read –
+        1. See the Timing Characteristics for the required delay between the
+        end of the RREG command and the beginning of shifting data on DOUT: t6.
+
+        TODO: Implement 2nd command byte
         """
 
         result = []
 
         # Pull the SPI bus low
-        bcm.gpio_write(self.CS, bcm.LOW)
+        self.cs_low()
         
         # Send the byte command
         self.SendByte(self.CMD_RREG | reg)
-
-        # Clear the bus
         self.SendByte(0x00)
+
+        # Wait for appropriate data delay
         self.DataDelay()
 
         # Read the register contents
         read = self.ReadByte()
 
         # Release the SPI bus
-        bcm.gpio_write(self.CS, bcm.HIGH)
+        self.cs_high()
 
         return read
 
-    def ReadData(self):
+    def WriteReg(self, start_register, data):
         """
-        Reads ADC data
+        Writes data to the register, implements: 
+        
+        WREG: Write to Register
+
+        Description: Write to the registers starting with the register
+        specified as part of the command. The number of registers that
+        will be written is one plus the value of the second byte in the
+        command.
+
+        1st Command Byte: 0101 rrrr where rrrr is the address to the first
+        register to be written.
+        
+        2nd Command Byte: 0000 nnnn where nnnn is the number of bytes to be
+        written – 1.
+        
+        TODO: Implement multiple register write
+        """
+
+        # Select the ADS chip
+        self.cs_low()
+
+        # Tell the ADS chip which register to start writing at
+        self.SendByte(CMD_WREG | register)
+
+        # Tell the ADS chip how many additional registers to write
+        self.SendByte(0x00)
+
+        # Send the data
+        self.SendByte(data)
+
+        # Release the ADS chip
+        self.cs_high()
+
+    def ReadADC(self):
+        """
+        Reads ADC data, implements:
+
+        RDATA: Read Data
+
+        Description: Issue this command after DRDY goes low to read a single
+        conversion result. After all 24 bits have been shifted out on DOUT,
+        DRDY goes high. It is not necessary to read back all 24 bits, but DRDY
+        will then not return high until new data is being updated. See the
+        Timing Characteristics for the required delay between the end of the
+        RDATA command and the beginning of shifting data on DOUT: t6
         """
 
         # Pull the SPI bus low
-        bcm.gpio_write(self.CS, bcm.LOW)
+        self.cs_low()
+
+        # Wait for data to be ready
+        self.WaitDRDY()
 
         # Send the read command
         self.SendByte(self.CMD_RDATA)
+        
+        # Wait through the data pause
         self.DataDelay()
 
         # The result is 24 bits
@@ -175,74 +390,21 @@ class
         result.append(self.ReadByte())
 
         # Release the SPI bus
-        bcm.gpio_write(self.CS, bcm.HIGH)
+        self.cs_high()
 
         # Concatenate the bytes
-        total = result[0] << 16
-        total |= result[1] << 8
+        total  = (result[0] << 16)
+        total |= (result[1] << 8)
         total |= result[2]
 
         return total
 
-    def CfgADC(self, gain, data_rate):
-        """
-        Configures the gain and data rate of the ADS chip
-        
-        Status Register:
-        Bit 0: DRDY - Data Ready
 
-            Read only
-
-        Bit 1: BUFEN - Analog Input Buffer Enable
-
-            0 = Buffer disabled (default)
-            1 = Buffer enabled
-
-        Bit 2: ACAL - Auto-calibrate
-
-            0 = Auto-calibration disabled (default)
-            1 = Auto-calibration enabled
-            
-            When auto-calibration is enabled self-calibration begins at the
-            completion of the WREG command that changes the PGA (bits 0-2 of
-            ADCON register), DR (bits 7-0 in the DRATE register), or BUFEN
-            (bit 1 of the STATUS register) values.
-
-        Bit 3: ORDER - Data Output Bit Order
-            
-            0 = Most significant bit first (default)
-            1 = Least significant bit first
-
-            Input data is always shifted in most significant byte and bit
-            first. Output data is always shifted out most significant byte
-            first. The ORDER bit controls the bit order of the output data
-            within the byte.
-
-        Bits 4-7: ID0, ID1, ID2, ID3 - Factory Programmed ID Bits
-
-            Read only
-
-
-        """
-        self.CURRENT_GAIN = gain
-        self.CURRENT_DRATE = data_rate
-
-        self.WaitDRDY()
-        
-
-        # Disable the internal buffer
-        parameter1 = 0
-        parameter1 = 1 << 2 # ORDER
-
-        
-
-
-
-    def ReadID(self):
-        """
-        Read the ID from the ADS chip
-        :returns: numeric identifier of the ADS chip
-        """
+    def ReadID(self): 
+    """
+    Read the ID from the ADS chip
+    :returns: numeric identifier of the ADS chip
+    """
         self.WaitDRDY()
         id = ReadReg(self.REG_STATUS)
-        return id >> 4
+        edeturn id >> 4
